1f46968690f78fc87c58aaee92153df7a30d6d5f
lly_cloud <lly_cloud@qq.com>
2019-11-26 05:07:39 +0800

    添加knn算法要实现的两个函数的接口

---
+++ knn.py
@@ -0,0 +1,20 @@
+
+
+def recognize(vector):
+    """
+    识别种子类别
+    :param vector: 要识别的种子的向量
+    :return: 返回识别的种子类别的编号（1,2,3）
+    """
+    pass
+
+
+def train(vectors, answers):
+    """
+    识别种子类别
+    :param vectors: 一个列表，每个vector[i]代表一个种子的向量
+    :param answers: answers[i]代表vectors[i]种子类别（1，2，3）
+    :return: 无返回值
+    """
+    pass
+
